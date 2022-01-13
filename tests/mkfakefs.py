b12a637014adcf940b8dd06a13fbdbf2be484cad
Terry Brown <terrynbrown@gmail.com>
2022-01-13 17:11:21 -0600

    Make makefilehier() spread dupes through folders
    Previously it placed them all in the same folder as the original.

--- tests/mkfakefs.py
+++ tests/mkfakefs.py
@@ -1,22 +1,22 @@
+import random
 from pathlib import Path
 from random import choices, randint
-import random
 
 MAXDEPTH = 4
 FILES = (3, 8)
-DUPE_PCT = 5
+DUPE_PCT = 10
 DIRS = (0, 3)
 SIZE = (0, 100000)
 # use these for unusual characters in file names to test assumptions
-CHARS = ''.join(chr(i) for i in range(ord(' '), ord('z') + 1)) + '€ह𐍈¢☐☑☒'
+CHARS = "".join(chr(i) for i in range(ord(" "), ord("z") + 1)) + "€ह𐍈¢☐☑☒"
 # or these for simpler file names
-CHARS = ''.join(chr(i) for i in range(ord('a'), ord('z') + 1))
-CHARS = CHARS.replace('\\', '').replace('/', '')
+CHARS = "".join(chr(i) for i in range(ord("a"), ord("z") + 1))
+CHARS = CHARS.replace("\\", "").replace("/", "")
 NAMELEN = (1, 20)
-SEED = 'RANDOMFILESYSTEM60923584'
+SEED = "RANDOMFILESYSTEM60923584"
 
 
-def makefilehier(base, seed=None, _depth=0):
+def makefilehier(base, seed=None, _depth=0, _dirs=None, _files=None):
     """Create a random file hierarchy
 
     Args:
@@ -30,21 +30,32 @@ def makefilehier(base, seed=None, _depth=0):
         random.seed(seed)
     base = Path(base)
     base.mkdir(exist_ok=True)
+    if _depth == 0:
+        _dirs = []  # list of dirs created
+        _files = []  # list of files created
     for file_i in range(0, randint(*FILES)):
-        name = ''.join(choices(CHARS, k=randint(*NAMELEN)))
+        name = "".join(choices(CHARS, k=randint(*NAMELEN)))
         size = randint(*SIZE)
-        data = ''.join(choices(CHARS, k=size)).encode('utf-8')
-        with open(base.joinpath(name), 'wb') as out:
-            out.write(data)
-        if randint(0, 99) < DUPE_PCT:
-            with open(base.joinpath(name + '2'), 'wb') as out:
-                out.write(data)
+        data = "".join(choices(CHARS, k=size)).encode("utf-8")
+        filepath = base / name
+        _files.append(filepath)
+        filepath.write_bytes(data)
 
     for dir_i in range(0, randint(*DIRS)):
-        name = ''.join(choices(CHARS, k=randint(*NAMELEN)))
-        base.joinpath(name).mkdir(exist_ok=True)
+        name = "".join(choices(CHARS, k=randint(*NAMELEN)))
+        dirpath = base / name
+        _dirs.append(dirpath)
+        dirpath.mkdir(exist_ok=True)
         if _depth < MAXDEPTH:
-            makefilehier(base.joinpath(name), seed=False, _depth=_depth + 1)
+            makefilehier(
+                base=dirpath, seed=False, _depth=_depth + 1, _dirs=_dirs, _files=_files
+            )
+
+    if _depth == 0:
+        for filepath in _files:  # duplicate DUPE_PCT of files to random folders
+            if randint(0, 99) < DUPE_PCT:
+                dirpath = random.choice(_dirs)
+                (dirpath / (filepath.name + "2")).write_bytes(filepath.read_bytes())
 
 
 if __name__ == "__main__":
